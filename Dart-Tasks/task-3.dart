02593e982838d8bba1b97c214939ce06d0cc01b3
mikiyasalemayehu <124512589+mikiyasalemayehu-mekonen@users.noreply.github.com>
2023-12-06 10:09:21 +0300

    Update task-3.dart
    my next dart task

--- Dart-Tasks/task-3.dart
+++ Dart-Tasks/task-3.dart
@@ -1,58 +1,68 @@
+import 'dart:async';
 import 'dart:io';
-class Calculator{
-  var num1;
-  var num2;
-  Calculator(this.num1,this.num2);
-  double addition(){
-  var sum=num1+num2;
-    return sum ;
-   }
 
-  double substraction(){
-    var difference=num1-num2;
-    return difference;
-   }
-  double mutiplication(){
-    var product=num1*num2;
-    return product;
-   }
-   double division(){
-    var dividend=num1/num2;
-    return dividend;
-   }
-}
-void main(){
-print("*******************\n");
-print("1.ADDITION\n");
-print("2.SUBSTRACTION\n");
-print("3.MUTIPLICATION\n");
-print("4.DIVISION\n");
-print("*******************\n");
-  print("enter a choice: ");
-int? choice=int.parse(stdin.readLineSync()!);
-  print("enter the first number: ");
-double? num1=double.parse(stdin.readLineSync()!);
-  print("enter the second number: ");
- double? num2=double.parse(stdin.readLineSync()!);
-Calculator test=new Calculator(num1,num2);
-if(choice==1){
-print("SUM=$test.addition()");
-}
-else if(choice==2){
-print("Difference=${test.substraction()}");
-}
-else if(choice==3){
-print("Product=${test.mutiplication()}");
-}
-else if(choice==4){
-  if(num2==0){
-    print("Invalid Divider");
+class Calculator {
+  double addition(double num1, double num2) {
+    return num1 + num2;
   }
-  else{
-print("QUOTIENT=${test.division()}");
+
+  double subtraction(double num1, double num2) {
+    return num1 - num2;
+  }
+
+  double multiplication(double num1, double num2) {
+    return num1 * num2;
+  }
+
+  double division(double num1, double num2) {
+    if (num2 == 0) {
+      throw const FormatException('Divisor cannot be zero');
+    }
+    return num1 / num2;
   }
 }
-else{
-  print("INVALID INPUT");
-}
+
+Future<void> main() async {
+  final calculator = Calculator();
+
+  print("*******************");
+  print("1. ADDITION");
+  print("2. SUBTRACTION");
+  print("3. MULTIPLICATION");
+  print("4. DIVISION");
+  print("*******************");
+
+  stdout.write("Enter a choice: ");
+  final choice = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter the first number: ");
+  final num1 = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter the second number: ");
+  final num2 = double.parse(stdin.readLineSync()!);
+
+  double result;
+
+  try {
+    if (choice == 1) {
+      result = calculator.addition(num1, num2);
+    } else if (choice == 2) {
+      result = calculator.subtraction(num1, num2);
+    } else if (choice == 3) {
+      result = calculator.multiplication(num1, num2);
+    } else if (choice == 4) {
+      result = calculator.division(num1, num2);
+    } else {
+      throw Exception("Invalid choice");
+    }
+
+    await Future.delayed(Duration(seconds: 5));
+    print("Result: $result");
+  } catch (e) {
+    if (e is FormatException) {
+      print("Error: ${e.message}");
+    } else {
+      print("Error: $e");
+    }
+  }
 }
